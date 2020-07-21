2f1cca12e9e6fa8a44bdf9af343c2db59f84753c
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-21 15:59:13 -0700

    Update build.sh
    fix URL

--- .travis/appimage/build.sh
+++ .travis/appimage/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
 mkdir -p "$HOME/.ccache"
-docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache quriouspixel/build-environments:linux-appimage /bin/bash /yuzu/.travis/appimage/docker.sh
+docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache quriouspixel/yuzu:latest /bin/bash /yuzu/.travis/appimage/docker.sh
