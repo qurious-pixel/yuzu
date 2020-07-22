5e3e7e9a9b845e4e7fa18b774b8d4fa4221c1b83
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-22 15:43:24 -0700

    Update docker.sh
    new home

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
-echo ${PWD}
-cd $HOME
-echo ${PWD}
+ls $HOME
+cd home/travis/build/qurious-pixel/yuzu
+ls .
 
 mkdir build && cd build
 cmake .. -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DYUZU_USE_QT_WEB_ENGINE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DYUZU_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DUSE_DISCORD_PRESENCE=ON
