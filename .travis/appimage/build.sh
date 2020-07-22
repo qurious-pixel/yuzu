598a6c73123f160f69aed4a51f378ce9dd3604a6
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-22 13:05:18 -0700

    Update build.sh
    add tag

--- .travis/appimage/build.sh
+++ .travis/appimage/build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p "$HOME/.ccache"
 #docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache quriouspixel/rpcs3-docker:latest /bin/bash /yuzu/.travis/appimage/docker.sh
-docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache rpcs3/rpcs3-travis-xenial /bin/bash /yuzu/.travis/appimage/docker.sh
+docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache rpcs3/rpcs3-travis-xenial:1.4 /bin/bash /yuzu/.travis/appimage/docker.sh
