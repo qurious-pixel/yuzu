2516ca45ba252ed775937fcf4610d8fd30339cf6
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-22 13:52:08 -0700

    Update deps.sh
    yuzu docker

--- .travis/appimage/deps.sh
+++ .travis/appimage/deps.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -ex
 
 #docker pull quriouspixel/rpcs3-docker:latest
-docker pull rpcs3/rpcs3-travis-xenial:1.4
+#docker pull rpcs3/rpcs3-travis-xenial:1.4
+docker pull yuzuemu/build-environments:linux-fresh
