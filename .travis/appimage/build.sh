4df755d70b5cf226fe79bc59974b1da6c45ca119
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-22 15:04:41 -0700

    Update build.sh
    yuzu pull

--- .travis/appimage/build.sh
+++ .travis/appimage/build.sh
@@ -3,4 +3,5 @@
 mkdir -p "$HOME/.ccache"
 #docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache quriouspixel/rpcs3-docker:latest /bin/bash /yuzu/.travis/appimage/docker.sh
 #docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache rpcs3/rpcs3-travis-xenial:1.4 /bin/bash /yuzu/.travis/appimage/docker.sh
-docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.travis/appimage/docker.sh
+#docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.travis/appimage/docker.sh
+docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache quriouspixel/yuzu:latest /bin/bash /yuzu/.travis/appimage/docker.sh
