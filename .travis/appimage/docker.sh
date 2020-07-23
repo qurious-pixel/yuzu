c9f0b2e35b1d5f06ac02910f0064e2203c08b70c
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-23 09:40:47 -0700

    Update docker.sh
    capture error log

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -8,6 +8,8 @@ ls -al
 cmake /yuzu -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON 
 ninja
 
+cat /home/yuzu/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
+
 ccache -s
 
 ctest -VV -C Release
