95ac945f87c34f36a4bb61fc8b107fb09c30986e
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-29 13:23:52 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -1,22 +1,16 @@
 #!/bin/bash -ex
 
-whoami
 chown -R 1027:1027 /yuzu
 su yuzu
-whoami
+whoami 
 
 cd /yuzu
 
 mkdir build && cd build
 
-#cmake /yuzu -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DYUZU_USE_QT_WEB_ENGINE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DYUZU_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DUSE_DISCORD_PRESENCE=ON
 cmake /yuzu -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON 
 
 ninja
 
 
-cat /yuzu/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
-
-ccache -s
-
-ctest -VV -C Release
+#cat /yuzu/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
