857c39d671a315a2a1ebca4d093768e00e99d96e
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-29 13:24:53 -0700

    Update build.sh

--- .travis/appimage/build.sh
+++ .travis/appimage/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -ex
 
 mkdir -p "$HOME/.ccache"
-#docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache quriouspixel/rpcs3-docker:latest /bin/bash /yuzu/.travis/appimage/docker.sh
-#docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache rpcs3/rpcs3-travis-xenial:1.4 /bin/bash /yuzu/.travis/appimage/docker.sh
-#docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.travis/appimage/docker.sh
 docker run -u root -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache --name yuzu_appimage quriouspixel/yuzu:latest /bin/bash /yuzu/.travis/appimage/docker.sh
