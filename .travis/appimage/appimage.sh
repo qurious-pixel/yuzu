a931884e446ced79bea22213aed2c3d9e5ea5e98
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-07-30 09:14:10 -0700

    Update appimage.sh
    add update info

--- .travis/appimage/appimage.sh
+++ .travis/appimage/appimage.sh
@@ -33,7 +33,7 @@ cp /tmp/libssl.so.47 /tmp/libcrypto.so.45 /usr/lib/x86_64-linux-gnu/
 # /tmp/squashfs-root/AppRun $HOME/squashfs-root/usr/bin/yuzu -appimage -unsupported-allow-new-glibc -no-copy-copyright-files -no-translations -bundle-non-qt-libs
 /tmp/squashfs-root/AppRun $HOME/squashfs-root/usr/bin/yuzu -unsupported-allow-new-glibc -no-copy-copyright-files -no-translations -bundle-non-qt-libs
 export PATH=$(readlink -f /tmp/squashfs-root/usr/bin/):$PATH
-/tmp/squashfs-root/usr/bin/appimagetool $HOME/squashfs-root
+/tmp/squashfs-root/usr/bin/appimagetool $HOME/squashfs-root -u "gh-releases-zsync|qurious-pixel|yuzu|continuous|yuzu-x86_64.AppImage.zsync"
 
 mkdir $HOME/artifacts/
 mkdir -p /yuzu/artifacts/
