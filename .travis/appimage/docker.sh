aee6e35c2656bff757e341d569858061148b7d67
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-02 18:11:01 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-SOURCEURL=`base64 -d <<<"aHR0cHM6Ly93d3c3OS56aXBweXNoYXJlLmNvbS9kL2tQSVJCbGx4LzI1MTY1L21zdmMtc291cmNlLTIwMjAwNzMwLWVkZDg3Y2Y0MC50YXIueHo="`
+SOURCEURL=`base64 -d <<<"aHR0cHM6Ly93d3c3OS56aXBweXNoYXJlLmNvbS9kL2tQSVJCbGx4LzYzOTQvbXN2Yy1zb3VyY2UtMjAyMDA3MzAtZWRkODdjZjQwLnRhci54eg=="`
 
 ln -s /home/yuzu/.conan /root
 mkdir -p /tmp/source
@@ -8,11 +8,11 @@ cd /tmp/source
 curl -sLO $SOURCEURL
 tar -xf *.xf
 mv yuzu-*/ yuzu/
-cd /yuzu
+cd yuzu/
 
 mkdir build && cd build
 
-cmake /yuzu -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON 
+cmake .. -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON 
 
 ninja
 
