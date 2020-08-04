fc520137dfabe77b1ce6176820f61b7de3a5f390
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-04 11:53:09 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -22,11 +22,10 @@ patch -p1 < inject-git-info.patch
 msvc=$(echo "${PWD##*/}"|sed 's/.*-//')
 mkdir -p build && cd build
 
-#cp /yuzu/src/web_service/web_backend.cpp /tmp/source/yuzu/src/web_service/
-#cp /yuzu/src/input_common/sdl/sdl_impl.cpp /tmp/source/yuzu/src/input_common/sdl/
+cp /yuzu/src/web_service/web_backend.cpp /tmp/source/yuzu/src/web_service/
+cp /yuzu/src/input_common/sdl/sdl_impl.cpp /tmp/source/yuzu/src/input_common/sdl/
 
 cmake .. -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DTITLE_BAR_FORMAT_IDLE="yuzu Early Access $title" -DTITLE_BAR_FORMAT_RUNNING="yuzu Early Access $title | {3}" -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DGIT_BRANCH="HEAD" -DGIT_DESC="$msvc" -DUSE_DISCORD_PRESENCE=ON
-#cmake .. -G Ninja
 
 ninja
 
