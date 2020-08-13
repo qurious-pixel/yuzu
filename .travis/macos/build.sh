b235ea434bd04327a2414cf4de8d8a13dd2fcf4b
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-13 11:03:46 -0700

    Update build.sh
    ninja
    v.10.13

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-export MACOSX_DEPLOYMENT_TARGET=10.14
+export MACOSX_DEPLOYMENT_TARGET=10.13
 export Qt5_DIR=$(brew --prefix)/opt/qt5
 export UNICORNDIR=$(pwd)/externals/unicorn
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
@@ -11,7 +11,7 @@ export PATH="/usr/local/opt/ccache/libexec:$PATH"
 mkdir build && cd build
 cmake --version
 cmake .. -DYUZU_USE_BUNDLED_UNICORN=ON -DYUZU_USE_QT_WEB_ENGINE=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DYUZU_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} -DUSE_DISCORD_PRESENCE=ON
-make -j4
+ninja
 
 ccache -s
 
