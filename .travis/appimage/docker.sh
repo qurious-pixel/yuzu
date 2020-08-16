2f3752760035f88f0ccec38d8261bb31677e64f0
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-15 18:30:21 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -30,15 +30,15 @@ patch -p1 < inject-git-info.patch
 patch -p1 < mime-type.patch
 mkdir -p build && cd build
 
-cp /yuzu/src/web_service/web_backend.cpp /tmp/source/yuzu/src/web_service/
-cp /yuzu/src/input_common/sdl/sdl_impl.cpp /tmp/source/yuzu/src/input_common/sdl/
+#cp /yuzu/src/web_service/web_backend.cpp /tmp/source/yuzu/src/web_service/
+#cp /yuzu/src/input_common/sdl/sdl_impl.cpp /tmp/source/yuzu/src/input_common/sdl/
 cp /yuzu/src/input_common/udp/client.cpp /tmp/source/yuzu/src/input_common/udp/
 
 cmake .. -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DTITLE_BAR_FORMAT_IDLE="$title" -DTITLE_BAR_FORMAT_RUNNING="$title | {3}" -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DGIT_BRANCH="HEAD" -DGIT_DESC="$msvc" -DUSE_DISCORD_PRESENCE=ON
 
 ninja
 
-cat yuzu/build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
+cat /tmp/source//build/CMakeFiles/CMakeError.log | curl -F 'f:1=<-' ix.io
 
 cd /tmp
 #curl -sLO "https://raw.githubusercontent.com/qurious-pixel/yuzu/pineapple/.travis/appimage/appimage.sh"
