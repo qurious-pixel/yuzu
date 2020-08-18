64eb87ca6d6f16398f1f1a5e9451238427fa0747
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-18 11:51:05 -0700

    Update docker.sh
    libpulse-dev
    libasound2-dev

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -11,8 +11,8 @@ export PATH=$QT_BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 # APT Packages
-#apt-get update
-#apt install -y libglu1-mesa-dev
+apt-get update
+apt install -y libasound2-dev libpulse-dev
 
 
 ln -s /home/yuzu/.conan /root
