ae2ce5b70655ae90d31c7eef5b7608eb02e7c379
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-25 19:26:45 -0700

    Update docker.sh

--- .travis/appimage/docker.sh
+++ .travis/appimage/docker.sh
@@ -7,9 +7,13 @@ ln -s /home/yuzu/.conan /root
 
 cd /yuzu
 
+git clone https://github.com/yuzu-emu/yuzu-mainline.git
+cd yuzu-mainline/
+git submodule update --init --recursive
+
 mkdir build && cd build
 
-cmake /yuzu -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON 
+cmake .. -G Ninja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON 
 
 ninja
 
