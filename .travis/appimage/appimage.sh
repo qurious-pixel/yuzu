64920d12dd311a6a5be40e604de97e5fcde680ee
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-08-28 20:13:52 -0700

    Update appimage.sh

--- .travis/appimage/appimage.sh
+++ .travis/appimage/appimage.sh
@@ -50,7 +50,7 @@ cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 squashfs-root/usr/optional/libstdc++
 cp /tmp/zen/zenity squashfs-root/usr/bin/
 printf "#include <bits/stdc++.h>\nint main(){std::make_exception_ptr(0);std::pmr::get_default_resource();}" | $CXX -x c++ -std=c++2a -o $HOME/squashfs-root/usr/optional/checker -
 
-echo $TRAVIS_COMMIT > $HOME/squashfs-root/version.txt
+echo $TRAVIS_BUILD_ID > $HOME/squashfs-root/version.txt
 
 unset QT_PLUGIN_PATH
 unset LD_LIBRARY_PATH
