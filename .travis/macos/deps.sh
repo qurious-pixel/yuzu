5614591e6d220b0a3c3abf2fc3ee004a608976b5
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-29 16:50:56 -0700

    Update deps.sh

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -5,5 +5,7 @@ brew install p7zip qt5 sdl2 ccache conan ninja
 brew outdated cmake || brew upgrade cmake
 brew upgrade gcc
 pip3 install macpack
-brew install unicorn && UNICORN_QEMU_FLAGS="--python=`whereis python`" pip3 install unicorn
+port install autogen autoconf automake nano libtool libsdl patchutils subversion wget gmake
+PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+#brew install unicorn && UNICORN_QEMU_FLAGS="--python=`whereis python`" pip3 install unicorn
 #pip3 install conan
