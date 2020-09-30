5245c126fa4e2539139c9e3fadcad3d6054255a9
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-29 17:31:06 -0700

    Update build.sh

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -6,7 +6,12 @@ export MACOSX_DEPLOYMENT_TARGET=10.13
 export Qt5_DIR=$(brew --prefix)/opt/qt5
 #export UNICORNDIR=$(pwd)/externals/unicorn
 export UNICORNDIR=/usr/local/Cellar/unicorn/1.0.1
-export PATH="/usr/local/opt/ccache/libexec:$PATH"
+export PATH="/usr/local/opt/ccache/libexec:/usr/local/opt/llvm/bin:$PATH"
+
+export CC="clang"
+export CXX="clang++"
+export LDFLAGS="-L/usr/local/opt/llvm/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm/include"
 
 # TODO: Build using ninja instead of make
 mkdir build && cd build
