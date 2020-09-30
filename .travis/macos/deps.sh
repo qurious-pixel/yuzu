40e4446c1ccd6f72f328612a0b24fbbcd9e18e21
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-30 11:31:31 -0700

    Update deps.sh

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -4,4 +4,4 @@ brew update
 brew install p7zip qt5 sdl2 ccache conan ninja openssl pulseaudio llvm ffmpeg
 brew outdated cmake || brew upgrade cmake
 pip3 install macpack
-#pip3 install conan
+pip3 install conan==1.24.0
