3bfbc2ef94e5d0a78e60fbbdf6f297bd18fc0f30
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-30 16:01:49 -0700

    Update deps.sh

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -6,6 +6,8 @@ export PATH="/usr/local/sbin:$PATH"
 
 brew update
 brew cask uninstall --force java
+softwareupdate --all --install --force
+brew link gettext
 brew doctor
 brew install p7zip qt5 sdl2 ccache conan ninja ffmpeg llvm boost openssl pulseaudio mbedtls zstd openssl gpm pkgconfig
 brew outdated cmake || brew upgrade cmake
