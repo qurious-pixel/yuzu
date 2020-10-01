774e6735d89a00465794a833ec2a5640bf0b9977
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-01 13:05:29 -0700

    Update deps.sh

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -1,13 +1,17 @@
 #!/bin/sh -x
 
+export USE_CCACHE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ANALYTICS=1
 export PATH="/usr/local/sbin:$PATH"
 
 brew update --force
 
-#brew install p7zip sdl2 ccache ninja ffmpeg llvm pulseaudio mbedtls gpm pkgconfig
-#brew upgrade boost qt openssl@1.1 zstd
-#brew outdated cmake || brew upgrade cmake
+brew install p7zip sdl2 ccache ninja ffmpeg llvm pulseaudio mbedtls gpm pkgconfig
+brew upgrade boost qt openssl@1.1 zstd
+brew outdated cmake || brew upgrade cmake
 #brew upgrade gcc
-#pip3 install macpack
+pip3 install macpack
 #brew install unicorn && UNICORN_QEMU_FLAGS="--python=`whereis python`" pip3 install unicorn
-#pip3 install conan
+pip3 install conan
