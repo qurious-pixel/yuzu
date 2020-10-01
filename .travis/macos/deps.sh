5ced5bbe8e0b8d22fc58a4591d70e73273825619
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-01 12:04:50 -0700

    Update deps.sh

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -2,16 +2,10 @@
 
 export PATH="/usr/local/sbin:$PATH"
 
-#git -C $(brew --repo homebrew/core) fetch --unshallow
+brew upgrade --force
 
-#softwareupdate --all --install --force
-#brew cask pin java
-brew update --force
-#brew cask uninstall --force java
-brew link gettext
-#brew doctor
-#brew install p7zip sdl2 ccache conan ninja ffmpeg llvm pulseaudio mbedtls gpm pkgconfig
-brew upgrade boost qt openssl@1.1 zstd
+brew install p7zip sdl2 ccache ninja ffmpeg llvm pulseaudio mbedtls gpm pkgconfig
+#brew upgrade boost qt openssl@1.1 zstd
 brew outdated cmake || brew upgrade cmake
 #brew upgrade gcc
 pip3 install macpack
