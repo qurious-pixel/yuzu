07a6066379878c3f8c9acf0214ac39b2597c8edb
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-01 19:38:52 -0700

    Update deploy.sh

--- .travis/macos/deploy.sh
+++ .travis/macos/deploy.sh
@@ -2,5 +2,5 @@
 
 ls -al artifacts/
 wget -c https://github.com/tcnksm/ghr/releases/download/v0.13.0/ghr_v0.13.0_darwin_amd64.zip
-unzipzip ghr_v0.13.0_darwin_amd64.zip      
+unzip ghr_v0.13.0_darwin_amd64.zip      
 ghr_v0.13.0_darwin_amd64/ghr -recreate -n 'yuzu mac 10.14' -b "Travis CI build log ${TRAVIS_BUILD_WEB_URL}" mac_10_14 artifacts/
