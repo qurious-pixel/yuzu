8875ab0b21b13194b870846a9505abfb55c7db54
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-01 19:36:31 -0700

    Update deps.sh

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -1,7 +1,13 @@
-#!/bin/sh -ex
+#!/bin/sh -x
+
+export USE_CCACHE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ANALYTICS=1
+export PATH="/usr/local/sbin:$PATH"
 
 brew update
-brew install p7zip qt5 sdl2 ccache conan ninja openssl pulseaudio llvm ffmpeg mbedtls zstd openssl gpm pkgconfig
+brew install p7zip qt5 sdl2 ccache conan ninja pulseaudio llvm ffmpeg mbedtls zstd openssl gpm pkgconfig
 brew outdated cmake || brew upgrade cmake
 pip3 install macpack
 pip3 install conan==1.24.0
