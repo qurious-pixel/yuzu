0be2b77b87c24713aa9edae35575c579ee13cfbf
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-01 19:35:30 -0700

    Update build.sh

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 set -o pipefail
 
@@ -9,13 +9,13 @@ export PATH="/usr/local/opt/ccache/libexec:/usr/local/opt/llvm/bin:$PATH"
 
 export CC="clang"
 export CXX="clang++"
-export LDFLAGS="-L/usr/local/opt/llvm/lib"
-export CPPFLAGS="-I/usr/local/opt/llvm/include"
+export LDFLAGS="-L/usr/local/opt/llvm/lib -L/usr/local/opt/openssl@1.1/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm/include -I/usr/local/opt/openssl@1.1/include"
 
 # TODO: Build using ninja instead of make
 mkdir build && cd build
 cmake --version
-cmake .. -GNinja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON 
+cmake .. -GNinja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX"
 ninja
 
 ccache -s
