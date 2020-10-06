864eef9a218c5b7729fa47d242a772ad3d61b9ce
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-06 10:09:56 -0700

    Update build.sh

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-export MACOSX_DEPLOYMENT_TARGET=10.14
+export MACOSX_DEPLOYMENT_TARGET=10.13
 export Qt5_DIR=$(brew --prefix)/opt/qt5
 export UNICORNDIR=$(pwd)/externals/unicorn
 #export UNICORNDIR=/usr/local/Cellar/unicorn/1.0.1
@@ -33,9 +33,9 @@ export VULKAN_SDK="${PWD}/vulkan-sdk"
 mkdir build && cd build
 cmake --version
 #cmake .. -GNinja -DYUZU_USE_BUNDLED_UNICORN=ON -DYUZU_USE_QT_WEB_ENGINE=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DYUZU_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} -DUSE_DISCORD_PRESENCE=ON
-cmake .. -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX"
-#ninja
-make clang-format 
-make -j$(nproc)
+cmake .. -GNinja -DYUZU_USE_BUNDLED_UNICORN=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX"
+ninja
+#make clang-format 
+#make -j$(nproc)
 
 ccache -s
