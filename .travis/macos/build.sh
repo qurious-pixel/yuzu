61cc5e8e0bb21c8fd29f4054519b158d49115113
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-06 09:39:14 -0700

    Update build.sh

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -8,8 +8,10 @@ export UNICORNDIR=$(pwd)/externals/unicorn
 #export UNICORNDIR=/usr/local/Cellar/unicorn/1.0.1
 export PATH="/usr/local/opt/ccache/libexec:/usr/local/opt/llvm/bin:/usr/local/opt/openssl@1.1/bin:$PATH"
 
-export CC="clang"
-export CXX="clang++"
+#export CC="clang"
+#export CXX="clang++"
+export CC="gcc"
+export CXX="g++"
 export LDFLAGS="-L/usr/local/opt/llvm/lib -L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/llvm/include -I/usr/local/opt/openssl@1.1v/include"
 export CXXFLAGS='-std=c++17'
