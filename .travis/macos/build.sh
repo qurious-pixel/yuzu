618da2fe0e76ef0ab019609542508ed5304bf4ca
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-10-26 12:05:05 -0700

    Update build.sh
    Clang 10.13

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -7,8 +7,8 @@ export Qt5_DIR=$(brew --prefix)/opt/qt5
 export UNICORNDIR=$(pwd)/externals/unicorn
 export PATH="/usr/local/opt/ccache/libexec:/usr/local/opt/llvm/bin:$PATH"
 
-export CC="gcc"
-export CXX="g++"
+export CC="clang"
+export CXX="clang++"
 export LDFLAGS="-L/usr/local/opt/llvm/lib -L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/llvm/include -I/usr/local/opt/openssl@1.1/include"
 
